918ae025784d91369821dbfcc0fa5f1f1ad2d24a
James Tortorelli <jtortorelli03@icloud.com>
2025-09-27 14:15:46 -0500

    add icons

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import googleFonts from "lume/plugins/google_fonts.ts";
+import icons from "lume/plugins/icons.ts";
 
 const site = lume({
   src: "./src",
@@ -14,5 +15,6 @@ site.use(
       "https://fonts.google.com/share?selection.family=Montserrat:ital,wght@0,100..900;1,100..900|Noto+Serif+JP:wght@200..900|Petrona:ital,wght@0,100..900;1,100..900|Sono:wght@200..800",
   })
 );
+site.use(icons(/* Options */));
 
 export default site;
--- src/index.md
+++
@@ -1,9 +0,0 @@
----
-title: This is my website
----
-
-<h1 class="font-display">Welcome to my website</h1>
-
-<p class="font-content">This is my first page using **Lume**, a static site generator for Deno.</p>
-
-<p class="font-detail">I hope you enjoy it.</p>
