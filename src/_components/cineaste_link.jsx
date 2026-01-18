fc1ac293511f96e3a1b2d472dc26dad8d2739485
James Tortorelli <jtortorelli03@icloud.com>
2026-01-18 12:51:35 -0600

    Update cineaste link style

--- src/_components/cineaste_link.jsx
+++ src/_components/cineaste_link.jsx
@@ -1,7 +1,7 @@
 export default function ({ href, children }) {
   return (
     <a
-      class="underline decoration-gray-300 decoration-1 underline-offset-2 hover:cursor-pointer hover:text-red-700 hover:decoration-red-700"
+      class="underline decoration-red-700 decoration-1 underline-offset-2 hover:cursor-pointer hover:text-red-700 hover:decoration-red-700"
       href={href}
     >
       {children}
